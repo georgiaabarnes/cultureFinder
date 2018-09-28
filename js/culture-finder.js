88cd3ce319f8bd01848e1b9b690d1a1437a1eae0
georgiabarnes <georgia@osa.io>
2018-09-28 12:34:35 -0400

    fancy animations

--- js/culture-finder.js
+++ js/culture-finder.js
@@ -40,43 +40,71 @@
   $(window).scroll(navbarCollapse);
 
   $('#eventCreationBtn').click(function() {
-    $('#featureImg').attr('src', 'img/eventCreation.png');
-    $('.active').fadeOut(500).removeClass('active');
+    $('#featureImg').hide().attr('src', 'img/eventCreation.png').fadeIn(1000);
+    $('.active-text').hide().removeClass('active-text');
     $('.active-link').removeClass('active-link');
     $('#eventCreationBtn').addClass('active-link');
-    $('#eventCreation').fadeIn(2000).addClass('active');
+    $('#eventCreation').fadeIn(2000).addClass('active-text');
   })
 
   $('#eventDataBtn').click(function() {
-    $('#featureImg').attr('src', 'img/eventData.png');
-    $('.active').hide().removeClass('active');
+    $('#featureImg').hide().attr('src', 'img/eventData.png').fadeIn(1000);
+    $('.active-text').hide().removeClass('active-text');
     $('.active-link').removeClass('active-link');
     $('#eventDataBtn').addClass('active-link');
-    $('#eventData').fadeIn(2000).addClass('active');
+    $('#eventData').fadeIn(2000).addClass('active-text');
   })
 
   $('#eventSharingBtn').click(function() {
-    $('#featureImg').attr('src', 'img/eventSharing.png');
-    $('.active').hide().removeClass('active');
+    $('#featureImg').hide().attr('src', 'img/eventSharing.png').fadeIn(1000);
+    $('.active-text').hide().removeClass('active-text');
     $('.active-link').removeClass('active-link');
     $('#eventSharingBtn').addClass('active-link');
-    $('#eventSharing').fadeIn(2000).addClass('active');
+    $('#eventSharing').fadeIn(2000).addClass('active-text');
   })
 
   $('#artistDirectoryBtn').click(function() {
-    $('#featureImg').attr('src', 'img/ArtistDirectory.png');
-    $('.active').hide().removeClass('active');
+    $('#featureImg').hide().attr('src', 'img/ArtistDirectory.png').fadeIn(1000);
+    $('.active-text').hide().removeClass('active-text');
     $('.active-link').removeClass('active-link');
     $('#artistDirectoryBtn').addClass('active-link');
-    $('#artistDirectory').fadeIn(2000).addClass('active');
+    $('#artistDirectory').fadeIn(2000).addClass('active-text');
   })
 
   $('#publicArtBtn').click(function() {
-    $('#featureImg').attr('src', 'img/publicArt.png');
-    $('.active').hide().removeClass('active');
+    $('#featureImg').hide().attr('src', 'img/publicArt.png').fadeIn(1000);
+    $('.active-text').hide().removeClass('active-text');
     $('.active-link').removeClass('active-link');
     $('#publicArtBtn').addClass('active-link');
-    $('#publicArt').fadeIn(2000).addClass('active');
+    $('#publicArt').fadeIn(2000).addClass('active-text');
   })
+
+  if ($(window).width() > 1200) {
+    setTimeout(function(){
+      $("#transition-container").remove();
+      $('#mainNav').show();
+    }, 1000)
+  } else {
+    $('#mainNav').show();
+  }
+
+  $(window).scroll( function(){
+    
+    /* Check the location of each desired element */
+    $('.fademe').each( function(i){
+        
+        var bottom_of_object = $(this).position().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        
+        /* If the object is completely visible in the window, fade it it */
+        if( bottom_of_window > bottom_of_object ){
+            
+            $(this).animate({'opacity':'1'},1500);
+                
+        }
+        
+    }); 
+
+});
 
 })(jQuery); // End of use strict
